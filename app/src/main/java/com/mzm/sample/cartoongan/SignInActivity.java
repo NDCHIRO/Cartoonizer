5b3cd2b7a759d9cd1cb365025a9aaf852d3b683a
amr mohamed salah <amrmohamed81298@gmail.com>
2021-06-01 15:50:25 +0200

    authentication, cartoonGAN

---
+++ app/src/main/java/com/mzm/sample/cartoongan/SignInActivity.java
@@ -0,0 +1,108 @@
+package com.mzm.sample.cartoongan;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.mzm.sample.cartoongan.DashboardActivity;
+import com.mzm.sample.cartoongan.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private String password;
+    private String email;
+    private EditText emailEt,passwordEt;
+    private Button SignInButton;
+    private TextView SignUpTv;
+    private ProgressDialog progressDialog;
+    
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        //Initialize Firebase Auth
+        firebaseAuth= FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser()!=null)
+        {
+            //opening profile activity
+            Intent dashboardIntent = new Intent(this, MainActivity.class);
+            //dashboardIntent.putExtra("mail", firebaseAuth.getCurrentUser().getEmail());
+            startActivity(dashboardIntent);
+        }
+
+        emailEt=findViewById(R.id.email);
+        passwordEt=findViewById(R.id.password);
+        SignInButton=findViewById(R.id.login);
+        progressDialog=new ProgressDialog(this);
+        SignUpTv=findViewById(R.id.signUpTv);
+
+        SignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+
+        SignUpTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SignInActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    
+
+
+
+    private void Login() {
+        String email=emailEt.getText().toString();
+        String password=passwordEt.getText().toString();
+        if(TextUtils.isEmpty(email)){
+            emailEt.setError("Enter your email");
+            return;
+        }
+        else if(TextUtils.isEmpty(password)){
+            passwordEt.setError("Enter your password");
+            return;
+        }
+        progressDialog.setMessage("Please wait...");
+        progressDialog.show();
+        progressDialog.setCanceledOnTouchOutside(false);
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //final String current_user_id = firebaseAuth.getCurrentUser().getUid();
+                    Toast.makeText(SignInActivity.this,"Login Successfully",Toast.LENGTH_LONG).show();
+                    Intent intent=new Intent(SignInActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Toast.makeText(SignInActivity.this,"Sign In fail!",Toast.LENGTH_LONG).show();
+                }
+                progressDialog.dismiss();
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/mzm/sample/cartoongan/SignUpActivity.java
@@ -0,0 +1,117 @@
+package com.mzm.sample.cartoongan;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.mzm.sample.cartoongan.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    private EditText emailEt,passwordEt1,passwordEt2;
+    private Button SignUpButton;
+    private TextView SignInTv;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        firebaseAuth=FirebaseAuth.getInstance();
+        emailEt=findViewById(R.id.email);
+        passwordEt1=findViewById(R.id.password1);
+        passwordEt2=findViewById(R.id.password2);
+        SignUpButton=findViewById(R.id.register);
+
+
+        progressDialog=new ProgressDialog(this);
+        SignInTv=findViewById(R.id.signInTv);
+        SignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Register();
+            }
+        });
+        SignInTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(SignUpActivity.this, SignUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+
+
+
+    private void Register(){
+        String email=emailEt.getText().toString();
+        String password1=passwordEt1.getText().toString();
+        String password2=passwordEt2.getText().toString();
+        if(TextUtils.isEmpty(email)){
+            emailEt.setError("Enter your email");
+            return;
+        }
+        else if(TextUtils.isEmpty(password1)){
+            passwordEt1.setError("Enter your password");
+            return;
+        }
+        else if(TextUtils.isEmpty(password2)){
+            passwordEt2.setError("Confirm your password");
+            return;
+        }
+        else if(!password1.equals(password2)){
+            passwordEt2.setError("Different password");
+            return;
+        }
+        else if(password1.length()<6){
+            passwordEt1.setError("Length should be > 6");
+            return;
+        }
+        else if(!isVallidEmail(email)){
+            emailEt.setError("invalid email");
+            return;
+        }
+        progressDialog.setMessage("Please wait...");
+        progressDialog.show();
+        progressDialog.setCanceledOnTouchOutside(false);
+        firebaseAuth.createUserWithEmailAndPassword(email,password1).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(SignUpActivity.this,"Successfully registered",Toast.LENGTH_LONG).show();
+                    Intent intent=new Intent(SignUpActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else{
+                    Toast.makeText(SignUpActivity.this,"Sign up fail!",Toast.LENGTH_LONG).show();
+                }
+                progressDialog.dismiss();
+            }
+        });
+    }
+
+
+    private Boolean isVallidEmail(CharSequence target){
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+}
